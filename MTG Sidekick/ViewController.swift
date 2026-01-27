1d44a3c3c8dd1968b324f1bc103aa3a31dc16563
Travis <tmiddaugh065@gmailcom>
2026-01-27 12:35:52 -0500

    Initial Commit

---
+++ MTG Sidekick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTG Sidekick
+//
+//  Created by Travis on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
