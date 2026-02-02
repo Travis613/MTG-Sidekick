7caab851f75ab7469b69cd68d5345651ec2a95f8
Travis <tmiddaugh065@gmailcom>
2026-02-02 14:29:31 -0500

    Refactored the setup screen to work with the IPhone SE smaller screen size and made the interface for 2 player.

--- MTG Sidekick/ViewController.swift
+++ MTG Sidekick/ViewController.swift
@@ -13,11 +13,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-/*
-    @IBAction func continueButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name:"Main", bundle: nil)
-        let main_screen = storyboard.instantiateViewController(identifier:"main_screen")
-        self.present(main_screen, animated: true, completion: nil)
-    }*/
-    
 }
